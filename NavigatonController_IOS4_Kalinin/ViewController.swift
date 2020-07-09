c6b5f60250eb419e0457b7d3b5d2c4b99b5af972
Artem Kalinin <KalininArtemVal@gmail.com>
2020-07-09 23:48:37 +0300

    done

---
+++ NavigatonController_IOS4_Kalinin/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  NavigatonController_IOS4_Kalinin
+//
+//  Created by Калинин Артем Валериевич on 09.07.2020.
+//  Copyright © 2020 Калинин Артем Валериевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var launchTimeLable: UILabel!
+    @IBOutlet weak var launchTime: UILabel!
+    @IBOutlet weak var appearenceTimeLable: UILabel!
+    @IBOutlet weak var appearenceTime: UILabel!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpItem()
+        setLaunchTime()
+    }
+    
+    // Устанавливаем время каждый раз при открытии view
+    override func viewWillAppear(_ animated: Bool) {
+        setViewTime()
+    }
+    
+    // Устанавливаем кнопку Update
+    func setUpItem() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Update", style: .plain, target: self, action: #selector(update))
+    }
+    
+    //Устанавливаем время запуска приложения
+    func setLaunchTime() {
+        let currentDateTime = Date()
+        
+        let forrmatter = DateFormatter()
+        forrmatter.timeStyle = .medium
+        forrmatter.dateStyle = .long
+        
+        let dateTimeString = forrmatter.string(from: currentDateTime)
+        launchTime.text = dateTimeString
+        
+    }
+    
+    //Устанавливаем время экрана
+    func setViewTime() {
+        let currentDateTime = Date()
+        
+        let forrmatter = DateFormatter()
+        forrmatter.timeStyle = .medium
+        forrmatter.dateStyle = .long
+        
+        let dateTimeString = forrmatter.string(from: currentDateTime)
+        appearenceTime.text = dateTimeString
+    }
+    
+    // Устанавливаем работу кнопки Update
+    @objc func update() {
+        let simpleController = SimpleViewController()
+        show(simpleController, sender: self)
+    }
+}
+
---
+++ SimpleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SimpleViewController.swift
+//  NavigatonController_IOS4_Kalinin
+//
+//  Created by Калинин Артем Валериевич on 09.07.2020.
+//  Copyright © 2020 Калинин Артем Валериевич. All rights reserved.
+//
+
+import UIKit
+
+class SimpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
